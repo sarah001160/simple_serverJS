afdd1da7145d5e0909f9ef5c7fa2ae41e944ba31
sarah001160 <sarah001160@gmail.com>
2024-11-10 23:16:01 +0800

    server.js 新增簡易的 POST 功能
    -script.js (client端) 試著post api

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// 需使用這段，才可以post
+app.use(express.json());
+
 // 靜態文件
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API 路由(/api/message 可以自行命名)
 app.get('/api/message', (req, res) => {
   res.json({ message: 'Hello from the server!' });
+});
+
+//try POST 請求的 API 路由
+app.post('/api/echo', (req, res) => {
+  const { message } = req.body;  // 確保 express.json() 中間件已啟用
+  res.json({ echo: message });
 });
 
 // 後端伺服器server
